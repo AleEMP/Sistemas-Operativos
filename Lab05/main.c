f9934ba41ff244489c506189bf15988b55c91fa7
AleEMP <118490110+AleEMP@users.noreply.github.com>
2024-10-01 22:36:18 -0500

    Add files via upload

--- Lab05/main.c
+++ Lab05/main.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+void sighandler(int);
 
-int main() {
-	while (1) {
-		pause();
-	}
-	return 0;
+int main () {
+   signal(SIGINT, sighandler);
+
+   while(1) {
+      printf("Going to sleep for a second...\n");
+      sleep(1);
+   }
+   return(0);
+}
+
+void sighandler(int signum) {
+   printf("Caught signal %d, coming out...\n", signum);
 }
