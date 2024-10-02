5571d84e33be44b0a95e9f72bedddf317995dfec
AleEMP <118490110+AleEMP@users.noreply.github.com>
2024-10-02 13:15:09 -0500

    Update main.c

--- Lab 04/main.c
+++ Lab 04/main.c
@@ -12,11 +12,11 @@ int main(){
 	pid_t child_pid;
 	file=open("write.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	file2=open("read.txt",O_RDONLY);
-	dup2(file,STDOUT_FILENO);
-	dup2(file2,STDIN_FILENO);
 	child_pid = fork();
 	char * list []={"prog.exe",NULL};
        	if (child_pid == 0) {
+       		dup2(file,STDOUT_FILENO);
+		dup2(file2,STDIN_FILENO);
 		char *programa = "./prog.exe";
 		char *args[] = { "prog", NULL };
 		execv(programa, args);
