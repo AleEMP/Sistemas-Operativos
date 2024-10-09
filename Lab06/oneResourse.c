737209b6906e5dc5349d1d9cdc8b0e548686c6fb
AleEMP <118490110+AleEMP@users.noreply.github.com>
2024-10-08 19:44:08 -0500

    semaphoras

---
+++ Lab06/oneResourse.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+
+#define KEY 0x1111
+
+struct sembuf v = {0, +1, SEM_UNDO};
+
+int main() {
+    int id = semget(KEY, 1, 0666);
+    semop(id, &v, 1);
+    return 0;
+}
